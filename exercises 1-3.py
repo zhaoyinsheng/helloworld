93905eacbeb5af86fc8dcc8d69cac9d44ffaf6e5
Zhao.Yinsheng <zhaoyinsheng@gmail.com>
2019-12-27 13:38:58 +0800

    之前的一些py练习

---
+++ exercises 1-3.py
@@ -0,0 +1,28 @@
+### LESSON 1: if elif else
+
+#if guess==num:
+#	print("Current! \nBut no any prize!")
+#elif guess>num:
+#	print("maybe a little BIGGER")
+#else:
+#	print("maybe a little SMALLER")
+#print("DONE!")
+
+### LESSON 2:while else
+
+#guess=int(input("Enter a number:"))
+#while guess != num:
+#	if guess > num:
+#		print("BIGGER!\n")
+#	else:
+#		print("SMALL!\n")
+#
+#	print("you should try again!")
+#	guess=int(input("Enter a number again:"))
+#else:
+#	print("Current! it's DONE!")
+
+### LESSON 3:for loop
+
+for i in range(1,5):
+	print("The num is {}".format(i)),
---
+++ exercises 4.py
@@ -0,0 +1,9 @@
+### LESSON 4:functions
+
+def sayhello(i):
+	print("{}.Hello World for {} time".format(i-1,i))
+
+for i in range(1,5):
+	sayhello(i)
+
+import sys
